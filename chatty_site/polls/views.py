cb9b85f09eeff38ffeb4d847be64c223a04953e7
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-06-25 12:48:26 -0400

    properly handling secrets

--- chatty_site/polls/views.py
+++ chatty_site/polls/views.py
@@ -7,3 +7,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello pollster")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're look at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're look at the results of a question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
