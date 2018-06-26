1c39f1ae6637d59a67c2d993638425db5ca545c6
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-06-26 17:13:46 -0400

    part 5 done

--- chatty_site/polls/views.py
+++ chatty_site/polls/views.py
@@ -7,6 +7,7 @@ from django.http import (HttpResponse,
                          HttpResponseRedirect)
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
@@ -18,12 +19,19 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        now = timezone.now()
+        return Question.objects.filter(pub_date__lte=now).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
