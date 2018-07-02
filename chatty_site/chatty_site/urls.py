06a6d4632bef5f1388f9f8e90bc2b8aeb34c8062
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-07-02 10:51:25 -0400

    chatty loading html index through django view

--- chatty_site/chatty_site/urls.py
+++ chatty_site/chatty_site/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('sentiment/', include('sentiment.urls')),
     # path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
