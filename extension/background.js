f1e0c19632c866ec4f22b0e4f704088f0f2a29a7
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-07-02 08:40:05 -0400

    don't need extension

--- extension/background.js
+++
@@ -1,9 +0,0 @@
-console.log("Running Your Extension!!!!")
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab) {
-    let msg = {
-        txt: "hello"
-    }
-    chrome.tabs.sendMessage(tab.id, msg)
-}
