b5024b42a21ca7393e71ab77bab9cf19ebe66dd7
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-07-02 09:57:49 -0400

    turned off the polls app

--- chatty_site/chatty_site/settings.py
+++ chatty_site/chatty_site/settings.py
@@ -32,8 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
-    'sentiment.apps.SentimentConfig',
+    # 'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
