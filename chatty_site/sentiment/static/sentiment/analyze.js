a0e1f6b53fd374359d5b51d21b37b93607e5c9b2
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-07-05 12:24:56 -0400

    changed caching

--- chatty_site/sentiment/static/sentiment/analyze.js
+++ chatty_site/sentiment/static/sentiment/analyze.js
@@ -3,12 +3,19 @@ var options = [
     set1 = ['First Option', 'Second Option', 'Third Option']
 ];
 
+function requestAnalysis () {
+    var request = new XMLHttpRequest();
+    request.open('get', 'http://127.0.0.1:5000', false);
+    request.send();
+    return request.responseText
+}
+
 function makeUL(array) {
     var list = document.createElement('ul');
 
     for(var i = 0; i < array.length; i++) {
         var item = document.createElement('li');
-        item.appendChild(document.createTextNode(array[i]))
+        item.appendChild(document.createTextNode(array[i]));
         list.appendChild(item);
     }
 
@@ -16,8 +23,8 @@ function makeUL(array) {
 }
 
 function makeList() {
-    var list = makeUL(options[0])
-    ul = document.getElementById('analysisComments')
-    console.log(list)
-    ul.appendChild(list)
+    var list = makeUL(options[0]);
+    ul = document.getElementById('analysisComments');
+    console.log(list);
+    ul.appendChild(list);
 }
--- chatty_site/sentiment/views.py
+++ chatty_site/sentiment/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.cache import cache_control
 
+# the page doesn't get refreshed often enough to legitimize 
+# long caching.
+@cache_control(max_age=3)
 def index(request):
     template = loader.get_template('sentiment/index.html')
     context = {}
