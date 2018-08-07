49ba4e5f614f702472e6de9b30f72f47cdeb3fb6
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-08-07 14:09:41 -0400

    conf reorganized

--- chatty/conf.py
+++ chatty/conf.py
@@ -16,6 +16,7 @@ def _load_configs():
 
 
 def _parse_envs(configs):
+    # Get what environment we're in from shell variable 'ENV'
     try:
         env = os.environ['ENV'].lower()
         if env == '':
@@ -23,12 +24,26 @@ def _parse_envs(configs):
     except KeyError:
         raise EnvironmentError("ENV environment variable should be set")
 
+    # Load all configs into dictionary
     env_configs = dict()
     for k, v in configs.items():
         new_value_conf = v[env]
         env_configs[k] = new_value_conf
 
+    # if in production should be providing SECRET_KEY
+    try:
+        SECRET_KEY = os.environ['SECRET_KEY']
+        if SECRET_KEY == '':
+            raise EnvironmentError(
+                "SECRET_KEY environment variable should be set\n"
+                "to something more than an empty string"
+                )
+    except KeyError:
+        raise EnvironmentError("SECRET_KEY environment variable should be set")
+    env_configs['api']['SECRET_KEY'] = SECRET_KEY
+
+
     return env_configs 
-        
+
 
 conf = _parse_envs(_load_configs())
