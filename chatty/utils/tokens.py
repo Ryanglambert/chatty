77de9f3a3339059434cfbb8e4f6ecc40a3c63d8b
Ubuntu <ubuntu@ip-172-31-63-36.ec2.internal>
2018-09-05 03:08:56 +0000

    feature elimination and tried a bunch of ensembles

--- chatty/utils/tokens.py
+++ chatty/utils/tokens.py
@@ -91,6 +91,21 @@ def tokenize(string: str, tokenizers=[], sep='_'):
             yield token
 
 
+def tokenize_pipe(doc: spacy.tokens.doc.Doc, tokenizers=[], sep='-'):
+    tokens = []
+    for tokenizer in tokenizers:
+        for token in tokenizer(doc, sep=sep):
+            tokens.append(token)
+    return tokens
+
+
+def tokenize_as_list_pipe(*args, **kwargs):
+    tokens = []
+    for tok in tokenize_pipe(*args, **kwargs):
+        tokens.append(tok)
+    return tokens
+
+
 def tokenize_as_list(*args, **kwargs):
     "Wrapper for tokenize to return list instead of generator"
     tokens = []
@@ -161,14 +176,15 @@ def make_vocabulary(docs, tokenizers=[], chunksize=25, n_jobs=1, verbose=False):
     # names will be used in the savename
     tokenizer_names, tokenizers = zip(*tokenizers)
     # make tokenizer
-    tokenizer = partial(tokenize_as_list,
-                                  sep='_', tokenizers=tokenizers)
     start = time.time()
-    if n_jobs == -1:
-        vocab = parmap(tokenizer, docs, chunksize=chunksize, verbose=verbose)
-    elif n_jobs > 0:
-        vocab = parmap(tokenizer, docs, chunksize=chunksize, processes=n_jobs, verbose=verbose)
+    if n_jobs > 0:
+        tokenizer = partial(tokenize_as_list_pipe,
+                                      sep='_', tokenizers=tokenizers)
+        # vocab = parmap(tokenizer, docs, chunksize=chunksize, processes=n_jobs, verbose=verbose)
+        vocab = [tokenizer(i) for i in nlp.pipe(docs, batch_size=chunksize, n_threads=n_jobs)]
     else:
+        tokenizer = partial(tokenize_as_list,
+                                      sep='_', tokenizers=tokenizers)
         vocab = list(map(tokenizer, docs))
     total_time = time.time() - start
 
@@ -197,8 +213,18 @@ def lemma_ngram_2(doc: spacy.tokens.doc.Doc, sep='-'):
         yield tok
 
 
+def lemma_ngram_3(doc: spacy.tokens.doc.Doc, sep='-'):
+    for tok in ngramize(lemma(doc), ngrams=[3]):
+        yield tok
+
+
 def pos_ngram_2(doc: spacy.tokens.doc.Doc, sep='-'):
     for tok in ngramize(pos(doc), ngrams=[2]):
+        yield tok
+
+
+def pos_ngram_3(doc: spacy.tokens.doc.Doc, sep='-'):
+    for tok in ngramize(pos(doc), ngrams=[3]):
         yield tok
 
     
@@ -216,6 +242,20 @@ def chunk_pos_bigram(doc: spacy.tokens.doc.Doc, sep='-'):
     for chunk in doc.noun_chunks:
         yield sep.join(('CHK_ROOT_POS', chunk.root.pos_,
                         'CHK_ROOT_HEAD', chunk.root.head.pos_))
+
+
+def sixth_shot(use_cached_utterances=True, chunksize=100, n_jobs=1, verbose=False):
+    train, _, test, _ = data.get_data(use_cached=use_cached_utterances)
+    utterances = train['utter'].tolist()
+    tokenizers = [
+        ('chunk_pos_bigram', chunk_pos_bigram),
+        ('word', lemma),
+        ('lemma_ngram_2', lemma_ngram_2),
+        ('pos_ngram_2', pos_ngram_2),
+        ('lemma_ngram_3', lemma_ngram_3),
+        ('pos_ngram_3', pos_ngram_3),
+    ]
+    make_vocabulary(utterances, tokenizers=tokenizers, n_jobs=n_jobs, chunksize=chunksize, verbose=verbose)
 
 
 def fifth_shot(use_cached_utterances=True, chunksize=100, n_jobs=1, verbose=False):
@@ -280,5 +320,6 @@ if __name__ == '__main__':
     # second_shot(n_jobs=4, chunksize=1000, verbose=True)
     # third_shot(n_jobs=4, chunksize=1000, verbose=True)
     # fourth_shot(n_jobs=4, chunksize=1000, verbose=True)
-    fifth_shot(n_jobs=4, chunksize=1000, verbose=True)
+    # fifth_shot(n_jobs=4, chunksize=1000, verbose=True)
+    sixth_shot(n_jobs=20, chunksize=1000, verbose=True)
 
