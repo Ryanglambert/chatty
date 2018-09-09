3578c614cdd4bc44fa568405e30f4868c0de2ea1
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-09-09 19:57:29 -0400

    making things private that should be

--- chatty/model.py
+++ chatty/model.py
@@ -2,7 +2,7 @@ import pickle
 import os
 
 
-def get_model_path(classifier='speech_act'):
+def _get_model_path(classifier='speech_act'):
     base_path = os.path.dirname(os.path.realpath(__file__))
     base_path = os.path.dirname(os.path.dirname(base_path))
     base_path = os.path.join(base_path, "chatty")
@@ -12,14 +12,14 @@ def get_model_path(classifier='speech_act'):
 
 
 def save_model(clf, classifier='speech_act'):
-    model_path = get_model_path(classifier)
+    model_path = _get_model_path(classifier)
     print("saving: {}".format(model_path))
     with open(model_path, 'wb') as f:
         pickle.dump(clf, f)
 
 
 def load_model(classifier='speech_act'):
-    model_path = get_model_path(classifier)
+    model_path = _get_model_path(classifier)
     print("loading: {}".format(model_path))
     with open(model_path, 'rb') as f:
         return pickle.load(f)
--- chatty/speech_acts.py
+++ chatty/speech_acts.py
@@ -5,27 +5,28 @@ from chatty import model
 from chatty.utils import tokens
 
 
-_tokenizer = partial(tokens.tokenize_as_list, tokenizers=[tokens.chunk_pos_bigram,
-                                                          tokens.sentence_subj_verb_obj,
-                                                          tokens.lemma])
+_tokenizer = partial(tokens.tokenize_as_list,
+                     tokenizers=[tokens.chunk_pos_bigram,
+                                 tokens.sentence_subj_verb_obj,
+                                 tokens.lemma])
 clf = model.speech_act_classifier()
 
 
-def tokenize_utterances(utter_t_minus_1, utter):
+def _tokenize_utterances(utter_t_minus_1, utter):
     current = tokens.token_joiner(_tokenizer(utter))
     prev = tokens.token_joiner(_tokenizer(utter_t_minus_1))
     return np.array([prev, current]).reshape(1, -1)
 
 
-def get_lags(utterances, lag=1):
+def _get_lags(utterances, lag=1):
     lagged_utterances = [None for i in range(lag)]
     for i in range(len(utterances) - lag):
         lagged_utterances.append(utterances[i])
     return lagged_utterances
 
 
-def append_lags(utterances: list) -> list:
-    lagged_utterances = get_lags(utterances, lag=1)
+def _append_lags(utterances: list) -> list:
+    lagged_utterances = _get_lags(utterances, lag=1)
     prev_current_utterances = filter(lambda x: x[0] is not None, zip(lagged_utterances,
                                                                      utterances))
     parsed = []
@@ -34,8 +35,8 @@ def append_lags(utterances: list) -> list:
     return parsed
 
 def parse(utterances: list) -> dict:
-    utters_with_lags = append_lags(utterances)
-    tokenized = [tokenize_utterances(*i) for i in utters_with_lags]
+    utters_with_lags = _append_lags(utterances)
+    tokenized = [_tokenize_utterances(*i) for i in utters_with_lags]
     speech_acts = ["None"] + [clf.predict(toks)[0] for toks in tokenized]
     # first utterance doesn't get classified so these values are empty
     confs = [list(zip(clf.classes_, [0] * clf.classes_.shape[0]))]
